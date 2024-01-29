fe5bcf6716a97ec31907eee5096cceefc87afd4c
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 12:53:34 +0200

    App.tsx: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import ContactList from '@/components/contactList/ContactList';
+import AppRoutes from '@/router/AppRoutes';
 
 function App(): JSX.Element {
 	return (
-		<div className="container lg:w-1/2 mx-auto bg-light dark:bg-dark p-5">
-			<h1 className="font-bold text-4xl text-color-light dark:text-color-dark mt-8 mb-40">Contacts</h1>
-			<ContactList />
+		<div className="container lg:w-1/2 mx-auto bg-light dark:bg-dark p-5 lg:p-10">
+			<AppRoutes />
 		</div>
 	);
 }
