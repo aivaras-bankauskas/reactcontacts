a2c7151171992c7549894aca6df79cf2ad832512
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 12:43:43 +0200

    ContactItem.tsx: add router Link and slug

--- src/components/contactItem/ContactItem.tsx
+++ src/components/contactItem/ContactItem.tsx
@@ -1,14 +1,17 @@
 import Contact from '@/data/models/contactModel';
+import { Link } from 'react-router-dom';
 
-function ContactItem({ firstName, lastName }: Contact): JSX.Element {
+function ContactItem({ slug, firstName, lastName }: Contact): JSX.Element {
 	return (
 		<li className="flex justify-between gap-x-6 py-2.5">
-			<div className="flex min-w-0 gap-x-4">
-				<div className="flex min-w-0 gap-x-2">
-					<div className="font-semibold text-color-light dark:text-color-dark">{firstName}</div>
-					<div className=" text-color-light dark:text-color-dark">{lastName}</div>
+			<Link to={`/${slug}`}>
+				<div className="flex min-w-0 gap-x-4">
+					<div className="flex min-w-0 gap-x-2">
+						<div className="font-semibold text-color-light dark:text-color-dark">{firstName}</div>
+						<div className=" text-color-light dark:text-color-dark">{lastName}</div>
+					</div>
 				</div>
-			</div>
+			</Link>
 		</li>
 	);
 }
