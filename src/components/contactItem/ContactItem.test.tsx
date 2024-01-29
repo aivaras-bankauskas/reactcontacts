c83a9b86f5e3aa316e6ae91cd28bec30e697e900
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 12:42:02 +0200

    ContactItem.test.tsx: add slug to test data

--- src/components/contactItem/ContactItem.test.tsx
+++ src/components/contactItem/ContactItem.test.tsx
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import ContactItem from './ContactItem';
 import Contact from '@/data/models/contactModel';
 
 describe('ContactItem', () => {
+	const contact: Contact = { slug: 'john-doe', firstName: 'John', lastName: 'Doe' };
+
+	beforeEach(() => {
+		render(
+			<Router>
+				<ContactItem {...contact} />
+			</Router>
+		);
+	});
+
 	it('renders the contact\'s first and last name', () => {
-		const contact: Contact = { firstName: 'John', lastName: 'Doe' };
-
-		render(<ContactItem {...contact} />);
-
 		expect(screen.getByText('John')).toBeInTheDocument();
 		expect(screen.getByText('Doe')).toBeInTheDocument();
+	});
+
+	it('renders a link to the contact\'s page', () => {
+		expect(screen.getByRole('link')).toHaveAttribute('href', '/john-doe');
 	});
 });
