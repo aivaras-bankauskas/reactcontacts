87fef1444e576ed39cac16eacf51789bd1ff1dba
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 14:22:46 +0200

    ContactIndexPage.tsx: update component name

--- src/views/pages/ContactIndexPage.tsx
+++ src/views/pages/ContactIndexPage.tsx
@@ -1,11 +1,11 @@
-import ContactList from '@/components/contactList/ContactList';
+import ContactsList from '@/components/contactsList/ContactsList';
 
 function ContactIndexPage(): JSX.Element {
 	return (
-		<>
+		<div className='bg-light dark:bg-dark p-5 lg:p-10'>
 			<h1 className="font-bold text-4xl text-color-light dark:text-color-dark mt-8 mb-40">Contacts</h1>
-			<ContactList />
-		</>
+			<ContactsList />
+		</div>
 	);
 }
 
