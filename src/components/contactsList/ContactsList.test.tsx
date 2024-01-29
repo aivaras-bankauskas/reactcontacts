bc911e4b7b47568e627addebae86adf27e375e59
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 14:20:46 +0200

    ContactsList.tsx: code optimization

--- src/components/contactsList/ContactsList.test.tsx
+++ src/components/contactsList/ContactsList.test.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import ContactList from './ContactList';
+import ContactList from './ContactsList';
 import contacts from '@/data/mocks/contactsMock';
 
 describe('ContactList', () => {
@@ -22,7 +22,7 @@ describe('ContactList', () => {
 	});
 
 	it('groups contacts by the first letter of the first name', () => {
-		const sampleContacts = contacts.slice(0, 5); // Test the first 5 contacts
+		const sampleContacts = contacts.slice(0, 10);
 		const firstLetters = sampleContacts.map(contact => contact.firstName.charAt(0).toUpperCase());
 		const uniqueFirstLetters = [...new Set(firstLetters)];
 
@@ -32,7 +32,7 @@ describe('ContactList', () => {
 	});
 
 	it('renders a link to each contact\'s page', () => {
-		const sampleContacts = contacts.slice(0, 5); // Test the first 5 contacts
+		const sampleContacts = contacts.slice(0, 10);
 
 		sampleContacts.forEach(contact => {
 			const link = screen.getByRole('link', { name: `${contact.firstName} ${contact.lastName}` });
--- src/components/contactsList/ContactsList.tsx
+++ src/components/contactsList/ContactsList.tsx
@@ -1,4 +1,4 @@
-import ContactItem from '@/components/contactItem/ContactItem';
+import { Link } from 'react-router-dom';
 import contacts from '@/data/mocks/contactsMock';
 import Contact from '@/data/models/contactModel';
 
@@ -26,7 +26,16 @@ function ContactList(): JSX.Element {
 					<h3 className="font-semibold text-zinc-400 mt-6 mb-1">{letter}</h3>
 					<ul role="list" className="divide-y divide-divider-light dark:divide-divider-dark border-y border-divider-light dark:border-divider-dark">
 						{contacts.map((contact: Contact) => (
-							<ContactItem key={contact.id} slug={contact.slug} firstName={contact.firstName} lastName={contact.lastName} />
+							<li className="flex justify-between gap-x-6 py-2.5">
+								<Link to={`/${contact.slug}`}>
+									<div className="flex min-w-0 gap-x-4">
+										<div className="flex min-w-0 gap-x-2">
+											<div className="font-semibold text-color-light dark:text-color-dark">{contact.firstName}</div>
+											<div className=" text-color-light dark:text-color-dark">{contact.lastName}</div>
+										</div>
+									</div>
+								</Link>
+							</li>
 						))}
 					</ul>
 				</div>
