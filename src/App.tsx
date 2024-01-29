7549870c2571b2eff398efbb34d1256006cce8fc
aivaras <aivarasbankauskas@gmail.com>
2024-01-29 14:21:38 +0200

    App.tsx: remove background

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AppRoutes from '@/router/AppRoutes';
 
 function App(): JSX.Element {
 	return (
-		<div className="container lg:w-1/2 mx-auto bg-light dark:bg-dark p-5 lg:p-10">
+		<div className="container lg:w-1/2 mx-auto">
 			<AppRoutes />
 		</div>
 	);
